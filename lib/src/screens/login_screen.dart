061b49e288379fc6af77c0f0e922ede30bd12fad
MohamedMF2 <furious_fouad@yahoo.com>
2021-03-19 21:19:26 +0200

    impelemation of validation with mixins

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_stateful/src/mixins/validation_mixin.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -8,18 +9,30 @@ class LoginScreen extends StatefulWidget {
   }
 }
 
-class LoginScreenState extends State<LoginScreen> {
+class LoginScreenState extends State<LoginScreen> with ValidationMixin {
+  final formKey = GlobalKey<FormState>();
+
+  String email;
+  String password;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Form(
+        key: formKey,
         child: Column(
           children: [
             emailField(),
-            // passwordField(),
-            // submitButton(),
+            Container(
+              margin: EdgeInsets.only(top: 20.0),
+            ),
+            passwordField(),
+            Container(
+              margin: EdgeInsets.only(top: 20.0),
+            ),
+            submitButton(),
           ],
         ),
       ),
@@ -31,14 +44,36 @@ class LoginScreenState extends State<LoginScreen> {
       keyboardType: TextInputType.emailAddress,
       decoration:
           InputDecoration(labelText: 'Email Address', hintText: 'mo@gmail.com'),
+      validator: validateEmail,
+      onSaved: (String value) {
+        email = value;
+      },
     );
   }
 
-  // Widget passwordField() {
-  //   return;
-  // }
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: '********',
+      ),
+      validator: validatePassword,
+      onSaved: (String value) {
+        password = value;
+      },
+    );
+  }
 
-  // Widget submitButton() {
-  //   return;
-  // }
+  Widget submitButton() {
+    return ElevatedButton(
+      child: Text('Submit'),
+      onPressed: () {
+        if (formKey.currentState.validate()) {
+          formKey.currentState.save();
+          print('sent data to an api email=$email password=$password');
+        }
+      },
+    );
+  }
 }
